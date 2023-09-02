1e11611cc77a30037b7cc4307a7d1e673f2d2fa2
lee <tokee678@daum.net>
2023-09-03 00:00:39 +0900

    [Feat] 로그인화면 세팅

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 import '../styles/_Login.scss'
+import instagram from '../images/instagram.png'
 
 function Login() {
 
@@ -13,21 +14,24 @@ function Login() {
     <div>
       <div className='firstBoxContainer'>
         <div className='instagramLogo'>
-          <button onClick={gotoMain}>
-            instagram
+          <button 
+          className='instaLogoBtn'
+          type='button' 
+          onClick={gotoMain}>
+          <img className='instaLogo' alt='Logo' src={instagram}/>
             </button>
         </div>
         <div className='loginBox'>
           <input placeholder='전화번호, 사용자 이름 또는 이메일' /><br />
           <input placeholder='비밀번호' /><br/>
-          <button>로그인</button>
+          <button type='button'>로그인</button>
           <div>또는</div>
           <div>Facebook으로 로그인</div>
           <div>비밀번호를 잊으셨나요?</div>
         </div>
       </div>
       <div className='secondBox'>
-계정이 없으신가요? <button>가입하기</button>
+계정이 없으신가요? <button type='button'>가입하기</button>
       </div>
     </div>
   )
